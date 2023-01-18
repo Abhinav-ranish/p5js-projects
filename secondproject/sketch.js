0ba31cb9886d05bcfd6982b8cbdaab328c15790a
Abhinav <abhinav.ranish@outlook.com>
2023-01-17 22:51:48 -0700

    Create sketch.js

---
+++ secondproject/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(color('blue'));
+  square(200, 0, 200);
+  fill(color('green'));
+  circle(300, 300, 200);
+  fill(color('yellow'));
+  square(0, 200, 200);
+  fill(color('red'));
+  circle(100, 100, 200); 
+  
+
+}
