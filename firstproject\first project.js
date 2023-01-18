f3665729b68a3003875514e64c484a0896d32870
Abhinav <abhinav.ranish@outlook.com>
2023-01-17 22:50:26 -0700

    Create firstproject\first project.js

---
+++ firstproject\first project.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  square(200, 0, 200);
+  
+  circle(300, 300, 200);
+  
+  square(0, 200, 200);
+  
+  circle(100, 100, 200); 
+  
+
+}
